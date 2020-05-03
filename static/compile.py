c5e4f332e386816b8a15a84514027162674017f6
InnovativeInventor <root@max.fan>
2020-05-02 21:37:13 -0400

    Added shared header

--- static/compile.py
+++ static/compile.py
@@ -3,20 +3,24 @@ import glob
 
 files = glob.glob("*.html")
 pages = []
+imports = []
 for each_file in files:
     with open(each_file) as f:
         if "<!DOCTYPE html>" in next(f).rstrip():
             pages.append(each_file)
+        else:
+            imports.append(each_file)
 
 args = {}
-# navbar = str(Markup("navbar.html"))
-with open("navbar.html") as f:
-    navbar = f.read()
-args["navbar"] = navbar
+for each_import in imports:
+    name = each_import.split(".")[0]
+    with open(each_import) as f:
+        import_html = f.read()
+    args[name] = import_html
 
-with open("footer.html") as f:
-    footer = f.read()
-args["footer"] = footer
+# with open("footer.html") as f:
+    # footer = f.read()
+# args["footer"] = footer
 
 for each_file in pages:
     with open(each_file) as f:
