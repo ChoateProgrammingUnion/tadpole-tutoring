a1d08154a9c2eefe0132cc47e9cbb486feb48f0d
Michael Lang <mlang21@bu.edu>
2020-06-08 21:55:41 -0400

    fixed the email issue. Not sure why the original one didn't work

--- server/notify.py
+++ server/notify.py
@@ -2,13 +2,13 @@ import smtplib
 import validators
 import config
 from email.message import EmailMessage
+from utils.log import log_info
 
 
 class Email:
     def __init__(self):
-        self.smtp = smtplib.SMTP_SSL('smtp.mail.us-east-1.awsapps.com', port=465)
-        self.smtp.login(config.EMAIL_USERNAME, config.EMAIL_PASSWORD)
-
+        pass
+        
     def send(self, recipient: str, subject = "", msg = "", reminder = True):
         """
         Sends a message
@@ -29,3 +29,36 @@ class Email:
             # msg = "Subject: " + subject.rstrip() + "\n" + msg.rstrip()
             # print(msg)
             # self.smtp.sendmail(config.EMAIL_USERNAME, recipient, msg)
+
+    def send2(recipient,subject,msg="",reminder=True): # V2 not really sure why V1 doesn't work
+        email = EmailMessage() # create the email message object    
+        if reminder: #detect type of email that was requested
+            email.set_content("This is a reminder for your tutoring session")
+            email["Subject"] = "Tutoring Reminder"
+            email["From"] = "noreply@tadpoletutoring.org"
+            email["To"] = recipient
+            email["BCC"] = "tadpoletutoring123@gmail.com" 
+        else:
+            email.set_content(msg) 
+            email["Subject"] = subject
+            email["From"] = "noreply@tadpoletutoring.org"
+            email["To"] = recipient
+            email["BCC"] = "tadpoletutoring123@gmail.com"
+        try:
+            mail = smtplib.SMTP_SSL('smtp.mail.us-east-1.awsapps.com', port=465)
+        except Exception as e:
+            log_info(e)
+            return "login error"
+        try:
+            mail.login(config.EMAIL_USERNAME, config.EMAIL_PASSWORD)
+        except Exception as e:
+            log_info(e)
+            return "wrong password/username"
+        try:
+            mail.send_message(email)
+        except Exception as e:
+            log_info(e)
+            return "send mail error"
+        return True
+        mail.quit()
+
